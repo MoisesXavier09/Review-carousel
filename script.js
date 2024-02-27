1e81dedf9420a330d23d85b4bfbac842aea41e52
MoisesXavier09 <156130689+MoisesXavier09@users.noreply.github.com>
2024-02-27 02:46:57 -0300

    feat: adiciona dados da equipe

---
+++ script.js
@@ -0,0 +1,32 @@
+const equipe = [
+    {
+      nome: "Ana Silva",
+      cargo: "Desenvolvedora Front-end",
+      texto: "Ana é uma desenvolvedora front-end experiente com paixão por criar interfaces de usuário intuitivas e elegantes. Ela tem um forte conhecimento de HTML, CSS e JavaScript.",
+      foto: "https://www.instagram.com/anasilvaportraits/?hl=en",
+    },
+    {
+      nome: "João Oliveira",
+      cargo: "Desenvolvedor Back-end",
+      texto: "João é um desenvolvedor back-end experiente com habilidades em diversas linguagens de programação, como Python, Java e Node.js. Ele é apaixonado por construir APIs robustas e escaláveis.",
+      foto: "https://www.instagram.com/joaotiagoliveira/?hl=en",
+    },
+    {
+      nome: "Maria Costa",
+      cargo: "Designer UX/UI",
+      texto: "Maria é uma designer UX/UI talentosa com um olho para a estética e a usabilidade. Ela tem experiência em criar designs de interface de usuário intuitivos e atraentes para websites e aplicativos.",
+      foto: "https://www.instagram.com/_u/marialazcosta/?utm_source=ig_embed&ig_rid=7fee0ff9-9862-4132-b64b-25ae0f9d31d0&ig_mid=4E9D34A0-93A3-4DC3-A80D-E2DC576DBB06",
+    },
+    {
+      nome: "Pedro Souza",
+      cargo: "Gerente de Projetos",
+      texto: "Pedro é um gerente de projetos experiente com habilidades em planejamento, organização e execução de projetos. Ele é apaixonado por ajudar equipes a alcançar seus objetivos.",
+      foto: "https://www.instagram.com/pedrosouza09/?hl=en",
+    },
+    {
+      nome: "Mariana Santos",
+      cargo: "Especialista em Marketing",
+      texto: "Mariana é uma especialista em marketing experiente com habilidades em marketing digital, branding e comunicação. Ela é apaixonada por ajudar empresas a crescer e alcançar seus objetivos de marketing.",
+      foto: "https://www.purepeople.com.br/famosos/mariana-santos_p3377/fotos/1",
+    },
+  ];
